779912aa0b147750be249107e236fc29f8926229
anela <anela89@gmail.com>
2022-06-08 15:58:07 -0700

    pseudo-code for app.py

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+# base.hmtl: title & body
+
+# localhost:5000/
+
+# 5 lines/inputs ****maybe some flexibility here??***
+
+# {{ {noun} }} <inputs name='noun'>
+# {{ {verb} }}
+
+# <submit button action='/results' method='??'>
