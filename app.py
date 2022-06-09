a8291260c3f51d09d2aa6b1fdf0ae3d4b29fb0a1
anela <anela89@gmail.com>
2022-06-08 18:35:30 -0700

    added get request to generate story, needs debugging

--- app.py
+++ app.py
@@ -21,5 +21,31 @@ def index():
 
     prompts = silly_story.prompts
 
-
     return render_template("questions.html", prompts = prompts)
+
+
+@app.get('/results')
+def create_story():
+    """Create MadLib from user input"""
+
+    # create dict from {prompt: input}
+    # silly_story.generate(dict)
+    # return render_template(story.html, )
+
+    # dictionary keys = <input name="{{prompt}}"
+    # dictionary values = value of the input itself
+
+    prompts = silly_story.prompts
+
+    ans = {}
+
+    for prompt in prompts:
+        ans[prompt] = request.args.get(f"{prompt}")
+
+    # noun = request.args["noun"]
+
+    #  = request.args.get()
+
+    story = silly_story.generate(ans)
+
+    return render_template("story.html", story=story)
